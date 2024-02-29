dd82ebb75b15dad188971f3686cdd99fd512566a
boden1501 <133118433+boden1501@users.noreply.github.com>
2024-02-29 20:44:24 +0700

    Add files via upload

---
+++ ProjectSem4/src/main/java/exam/spring/demo/ProjectSem4Application.java
@@ -0,0 +1,13 @@
+package exam.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSem4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSem4Application.class, args);
+	}
+
+}
